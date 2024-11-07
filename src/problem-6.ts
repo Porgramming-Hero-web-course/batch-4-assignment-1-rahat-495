9803b3d845796322733a2dc8076efaa7b5e4a595
rahat-495 <md8702687@gmail.com>
2024-11-07 15:06:35 +0600

    problem-6 are solved !

---
+++ src/problem-6.ts
@@ -0,0 +1,14 @@
+
+interface Profile {
+    age : number ;
+    name : string ; 
+    email : string ;
+}
+
+const updateProfile = (obj : Profile , updatedObj : object) => {
+    obj = { ...obj , ...updatedObj } ;
+    return obj ;
+}
+
+const myProfile = { name: "Alice", age: 25, email: "alice@example.com" };
+console.log(updateProfile(myProfile, { age: 26 }));
